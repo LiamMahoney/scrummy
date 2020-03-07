87ce8b0a45da1f701ff114396e691f90acd0e86a
Liam Mahoney <liammahoney96@gmail.com>
2020-03-07 06:30:44 +0000

    going to explicitly define exports

--- core/actions/label.js
+++ core/actions/label.js
@@ -1,11 +1,13 @@
+/**
+ * Creates a new label in the format `type: title`.
+ * 
+ * @param {string} type: the label type
+ * @param {string} title: the laebl title
+ */
+function createLabel(type, title) {
+    console.log(`creating label '${type}: ${title}`);
+}
+
 module.exports = {
-    /**
-     * Creates a new label in the format `type: title`.
-     * 
-     * @param {string} type: the label type
-     * @param {string} title: the laebl title
-     */
-    createLabel: function (type, title) {
-        console.log(`creating label '${type}: ${title}`);
-    }
+    createLabel
 }
