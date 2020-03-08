527ef4abddcd2a418b5025975e0b5d4aa7f7ef3f
Liam Mahoney <liammahoney96@gmail.com>
2020-03-08 01:26:41 +0000

    create label action

--- core/actions/label.js
+++ core/actions/label.js
@@ -1,12 +1,38 @@
+const request = require('../../utils/request');
+
 /**
  * Creates a new label in the format `type: title`.
  * 
  * @param {string} type: the label type
  * @param {string} title: the laebl title
+ * @param {Object} repo: repository information {name: "repository name", owner: "owner github login"}
  */
-function createLabel(type, title) {
-    //TODO:
-    console.log(`creating label '${type}: ${title}'`);
+function createLabel(type, title, repo) {
+
+    let data = {
+        name: `${type}: ${title}`,
+        color: genColor()
+    }
+
+    let options = {
+        path: `/repos/${repo.owner}/${repo.name}/labels`
+    }
+
+    request.post(options, data).then((res) => {
+        if (res.statusCode !== 201) {
+            throw `expected 201 but recieved ${res.statusCode} ${res.method} ${res.path} ${res.data}`;
+        }
+    }).catch((err) => {
+        throw err.stack;
+    });
+}
+
+/**
+ * Gnerates a random color hex string
+ * @returns {string}: hex representation of the color
+ */
+function genColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
 }
 
 module.exports = {
