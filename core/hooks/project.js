fc32da2227b96f3958ef0f9e69bcbcd487c94a0d
Liam Mahoney <liammahoney96@gmail.com>
2020-03-08 18:48:08 +0000

    documentation

--- core/hooks/project.js
+++ core/hooks/project.js
@@ -1,5 +1,12 @@
 const { Label } = require('../actions');
 
+/**
+ * Figures out which action is needed based on the github proejct webook
+ * recieved. This function only deals with project webhooks. Actions 
+ * are decided based on the data.action (what happened in github).
+ * 
+ * @param {Object} data: github webhook json data
+ */
 function project(data) {
     return new Promise((resolve, reject) => {
         console.debug(`project recieved action: ${data.action}`);
