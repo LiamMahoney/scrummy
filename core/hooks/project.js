0e11db7be400528e65a1354c4abfda6b71aa7afc
Liam Mahoney <liammahoney96@gmail.com>
2020-03-08 01:26:55 +0000

    passing repo data

--- core/hooks/project.js
+++ core/hooks/project.js
@@ -2,9 +2,14 @@ const { Label } = require('../actions');
 
 function project(data) {
     console.debug(`project recieved action: ${data.action}`);
+    let repo = {
+        owner: data.repository.owner.login,
+        name: data.repository.name
+    }
+
     switch (data.action) {
         case 'created':
-            Label.createLabel("project", data.project.body);
+            Label.createLabel("project", data.project.body, repo);
             break;
     }
 }
--- utils/request.js
+++ utils/request.js
@@ -1,2 +1,95 @@
 const https = require('https');
+const http = require('http');
+const log = require('./log');
+const config = require('../config/config');
 
+/**
+ * 
+ * @param {object} opts: various HTTP request options. See https://nodejs.org/docs/latest-v12.x/api/http.html#http_http_request_options_callback for valid options
+ */
+function get(opts) {
+    //TODO: validate options
+
+    let options = {
+        hostname: config.githubApiUrl,
+        port: 443,
+        path: opts.path,
+        method: 'GET',
+        headers: {
+            Authorization: `Basic ${Buffer.from(`${config.gitUser}:${config.gitPassword}`).toString('base64')}`,
+            'User-Agent': 'Scrummy'
+        }
+    }
+
+    let req = https.request(options, (res) => {
+        let data = '';
+        console.log(`response status code: ${res.statusCode}`);
+
+        res.on('data', (d) => {
+            data += d;
+        });
+
+        res.on('end', () => {
+            console.log(`data: ${data}`);
+        })
+    });
+
+    req.on('error', (err) => {
+        console.error(err);
+    });
+
+    req.end();
+}
+
+/**
+ * Sends a HTTP post request to the Github API.
+ * 
+ * @param {Object} opts: various HTTP request options
+ * @param {Object} data: post data to send 
+ */
+function post(opts, data) {
+    return new Promise((resolve, reject) => {
+        //TODO: validate options
+
+        let options = {
+            hostname: config.githubApiUrl,
+            port: 443,
+            path: opts.path,
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${config.gitUser}:${config.gitPassword}`).toString('base64')}`,
+                'User-Agent': 'Scrummy'
+            }
+        }
+
+        let req = https.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (d) => {
+                data += d;
+            }).on('end', function () {
+                resolve({
+                    data: JSON.parse(data),
+                    statusCode: this.statusCode,
+                    path: this.req.path,
+                    method: this.req.method
+                });
+            });
+
+        });
+
+        req.on('error', (err) => {
+            reject(err);
+        });
+
+        req.write(JSON.stringify(data));
+        req.end();
+    });
+}
+
+
+
+module.exports = {
+    get,
+    post
+}
