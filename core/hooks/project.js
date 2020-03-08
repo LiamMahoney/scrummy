5a42eb785d5dc1933e936fa4355071b756336767
Liam Mahoney <liammahoney96@gmail.com>
2020-03-08 17:19:30 +0000

    formatting

--- core/hooks/project.js
+++ core/hooks/project.js
@@ -3,6 +3,7 @@ const { Label } = require('../actions');
 function project(data) {
     return new Promise((resolve, reject) => {
         console.debug(`project recieved action: ${data.action}`);
+        
         let repo = {
             owner: data.repository.owner.login,
             name: data.repository.name
