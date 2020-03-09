b052b7dfb37f9a94f17c452e315bdcf8786ad64d
Liam Mahoney <liammahoney96@gmail.com>
2020-03-09 01:18:19 +0000

    intial issue creation + get issue

---
+++ core/actions/issue.js
@@ -0,0 +1,23 @@
+const request = require('../../utils/request');
+const url = require('url');
+
+
+/**
+ * Gets details about an issue given an api URL (which is typically
+ * given in an API request from github).
+ * 
+ * @param {String} url: Full API url to issue
+ */
+function getIssue(apiURL) {
+    return new Promise((resolve, reject) => {
+        let path = url.parse(apiURL);
+
+        let options = {
+            path: path.pathname
+        }
+
+        request.get(options).then((response) => {
+            resolve(response);
+        });
+    });
+}
