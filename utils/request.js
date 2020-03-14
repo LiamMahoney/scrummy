11550518c4a655dd3595fe5d151274dbbce7aebf
Liam Mahoney <liammahoney96@gmail.com>
2020-03-14 13:09:42 +0000

    graphql post

--- utils/request.js
+++ utils/request.js
@@ -97,6 +97,55 @@ function post(opts, data) {
     });
 }
 
+/**
+ * Sends a HTTP post request to the Github API.
+ * 
+ * @param {Object} opts: various HTTP request options
+ * @param {Object} data: post data to send
+ * @param {Function} callback: callback function to call when the request is done
+ */
+function graphQLPost(opts, data) {
+    //TODO: validate options
+    return new Promise((resolve, reject) => {
+        let options = {
+            hostname: config.githubApiUrl,
+            port: 443,
+            path: '/graphql',
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${config.gitUser}:${config.gitPassword}`).toString('base64')}`,
+                'User-Agent': 'Scrummy'
+            }
+        }
+        
+        //TODO: need to do the same with regular options
+        Object.assign(options.headers, opts.headers);
+
+        let req = https.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (d) => {
+                data += d;
+            }).on('end', function () {
+                return resolve({
+                    data: JSON.parse(data),
+                    statusCode: this.statusCode,
+                    path: this.req.path,
+                    method: this.req.method
+                });
+            });
+
+        });
+
+        req.on('error', (err) => {
+            return reject(err);
+        });
+
+        req.write(JSON.stringify(data));
+        req.end();
+    });
+}
+
 
 
 module.exports = {
