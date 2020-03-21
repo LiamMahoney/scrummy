5cf1233f021de7254a48e391b01409a2ba5c785d
Liam Mahoney <liammahoney96@gmail.com>
2020-03-21 22:24:14 +0000

    getProject method added

--- core/actions/project.js
+++ core/actions/project.js
@@ -1,5 +1,30 @@
 const request = require('../../utils/request');
+const url = require('url');
 
+/**
+ * Gets project information from the project URL returned 
+ * from the webhook.
+ * 
+ * @param {string} projURL URL for github GET project request - from webhook
+ */
+async function getProject(projURL) {
+    try {
+        let options = {
+            path: url.parse(projURL).path,
+            headers: {
+                Accept: 'application/vnd.github.inertia-preview+json'
+            }
+        }
+
+        let resp = await request.get(options);
+
+        return await request.handleRest(200, resp);
+
+    } catch (err) {
+        throw new Error(err.stack);
+    } 
+}
 
 module.exports = {
+    getProject
 }
