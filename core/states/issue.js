352aa588154e4b56ff26f611be1e49cd2bfeeb4a
Liam Mahoney <liammahoney96@gmail.com>
2020-03-22 16:33:15 +0000

    added methods findProjectLabel and issueRemovedFromProejct

--- core/states/issue.js
+++ core/states/issue.js
@@ -11,17 +11,56 @@ async function issueAddedToProject(data) {
         // checking that the project card is an instance of an issue
         // only project cards that are instances of issues have the content_url field
         if (data.project_card.content_url) {
-            proms = []
-            proms.push(Label.getAllLabels(data.repository.owner.login, data.repository.name));
-            proms.push(Project.getProject(data.project_card.project_url));
-            proms.push(Issue.getIssue(data.project_card.content_url));
-
-            let [labels, project, issue] = await Promise.all(proms);
-
-            let labelToAdd = await matchProjectLabel(project.name, labels);
+            let [issue, labelToAdd] = await findProjectLabel(data);
 
             return await Issue.addLabels(issue.number, [labelToAdd.name], data.repository.owner.login, data.repository.name);
         }
+    } catch (err) {
+        throw new Error(err.stack);
+    }
+}
+
+/**
+ * Removes the matching `project: <project>` label that the 
+ * issue was just removed from.
+ * 
+ * @param {Object} data webhook payload
+ */
+async function issueRemovedFromProject(data) {
+    try {
+        // checking that the project card is an instance of an issue
+        // only project cards that are instances of issues have the content_url field
+        if (data.project_card.content_url) {
+            let [issue, labelToRemove] = await findProjectLabel(data);
+
+            return await Issue.removeLabels(issue.number, [labelToRemove.name], data.repository.owner.login, data.repository.name);
+        }
+    } catch (err) {
+        throw new Error(err.stack);
+    }
+}
+
+/**
+ * Finds the matching `project: <project>` for the project
+ * the project card (instance of issue) was just added to
+ * or removed from.
+ * 
+ * @param {Object} data webhook payload
+ * @returns {Array} index 0 contains information about the issue,
+ * index 1 contains an object describing the matching project label
+ * to add or remove
+ */
+async function findProjectLabel(data) {
+    try {
+        proms = []
+        proms.push(Label.getAllLabels(data.repository.owner.login, data.repository.name));
+        proms.push(Project.getProject(data.project_card.project_url));
+        proms.push(Issue.getIssue(data.project_card.content_url));
+
+        let [labels, project, issue] = await Promise.all(proms);
+
+        return [issue, await matchProjectLabel(project.name, labels)];
+
     } catch (err) {
         throw new Error(err.stack);
     }
