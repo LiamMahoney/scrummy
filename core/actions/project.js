65b90900bdcff9ca9b5dc48e673dd5ad2d127aaa
Liam Mahoney <liammahoney96@gmail.com>
2020-03-25 00:05:34 +0000

    added getColumn method

--- core/actions/project.js
+++ core/actions/project.js
@@ -25,6 +25,31 @@ async function getProject(projURL) {
     } 
 }
 
+/**
+ * Gets project column information from the column URL returned 
+ * from the webhook.
+ * 
+ * @param {string} columnURL URL for github GET project column request - from webhook
+ */
+async function getColumn(columnURL) {
+    try {
+        let options = {
+            path: url.parse(columnURL).path,
+            headers: {
+                Accept: 'application/vnd.github.inertia-preview+json'
+            }
+        }
+
+        let resp = await request.get(options);
+
+        return await request.handleRest(200, resp);
+
+    } catch (err) {
+        throw new Error(err.stack);
+    } 
+}
+
 module.exports = {
-    getProject
+    getProject,
+    getColumn
 }
