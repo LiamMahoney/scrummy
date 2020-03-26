00a6e6ffb71693af5774a86af87b0e0fde826c63
Liam Mahoney <liammahoney96@gmail.com>
2020-03-26 00:49:02 +0000

    projectCreated state

--- core/states/project.js
+++ core/states/project.js
@@ -1,6 +1,21 @@
 const { Label } = require('../actions');
 
+/**
+ * Creates a `project: <project>` label for the project
+ * created.
+ * 
+ * @param {Object} data webhook payload
+ */
+async function projectCreated(data) {
+    try {
+        await Label.createLabel(data.repository.owner.login, data.repository.name, `project: ${data.project.name.toLowerCase()}`);
 
+        return `created label 'project: ${data.project.name.toLowerCase()}'`;
+    } catch (err) {
+        throw new Error(err.stack);
+    }
+}
 
 module.exports = {
+    projectCreated
 }
