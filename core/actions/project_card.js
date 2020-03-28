b38fa5b8a8d175b6b100f7e92cddd4203145e5c2
Liam Mahoney <liammahoney96@gmail.com>
2020-03-28 17:59:07 +0000

    added moveProjectCard method

--- core/actions/project_card.js
+++ core/actions/project_card.js
@@ -1,4 +1,42 @@
 const request = require('../../utils/request');
 
+/**
+ * Moves the project card to the specified column
+ * 
+ * @param {String} cardID project card ID
+ * @param {String} columnID column ID to move project card to 
+ */
+async function moveProjectCard(cardID, columnID) {
+    try {
+        let options = {
+            path: `/graphql`
+        }
+
+        let payload = {
+            mutation: `mutation {
+                moveProjectCard(input:{cardId: "${cardID}", columnId: "${columnID}"}) {
+                  cardEdge {
+                    node {
+                      id
+                      column {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            `
+        }
+
+        let resp = request.post(options, payload);
+
+        return await request.handleQL(resp);
+
+    } catch (err) {
+        throw new Error(err.stack);
+    }
+}
+
 module.exports = {
+    moveProjectCard
 }
