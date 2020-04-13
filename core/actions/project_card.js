320ba44b9334516eeb9fd9f5838201fde4fda760
Liam Mahoney <liammahoney96@gmail.com>
2020-04-13 00:39:22 +0000

    added deleteProjectCard

--- core/actions/project_card.js
+++ core/actions/project_card.js
@@ -3,7 +3,7 @@ const request = require('../../utils/request');
 /**
  * Moves the project card to the specified column
  * 
- * @param {String} cardID project card ID
+ * @param {String} cardID project card ID to move
  * @param {String} columnID column ID to move project card to 
  */
 async function moveProjectCard(cardID, columnID) {
@@ -15,16 +15,16 @@ async function moveProjectCard(cardID, columnID) {
         let payload = {
             query: `mutation {
                 moveProjectCard(input:{cardId: "${cardID}", columnId: "${columnID}"}) {
-                  cardEdge {
-                    node {
-                      id
-                      column {
-                        name
-                      }
+                    cardEdge {
+                        node {
+                            id
+                            column {
+                                name
+                            }
+                        }
                     }
-                  }
                 }
-              }`
+            }`
         }
 
         let resp = await request.post(options, payload);
@@ -36,6 +36,27 @@ async function moveProjectCard(cardID, columnID) {
     }
 }
 
+/**
+ * Delete the project card.
+ * 
+ * @param {String} cardID project card ID to remove
+ */
+async function deleteProjectCard(cardID) {
+    try {
+        let options = {
+            path: `/projects/columns/cards/${cardID}`,
+            headers: {
+                "Accept": "application/vnd.github.inertia-preview+json"
+            }
+        }
+
+        let resp = await request.del(options);
+
+        return await request.handleRest(204, resp);
+    } 
+}
+
 module.exports = {
-    moveProjectCard
+    moveProjectCard,
+    deleteProjectCard
 }
