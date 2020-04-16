b13cd0d74e2f947be38f5b5c003b240e026143e3
Liam Mahoney <liammahoney96@gmail.com>
2020-04-16 13:37:45 +0000

    added genericGet

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const config = require('../config/config');
+const url = require("url");
 
 /**
  * 
@@ -195,10 +196,33 @@ async function handleQL(resp) {
     }
 }
 
+/**
+ * Executes an API GET request given the url. Often the API responds with a
+ * URL to get more information about an object. This function executes that
+ * URL and returns the information.
+ * 
+ * @param {String} url the URL to get
+ */
+async function genericGet(url) {
+    try {
+        options = {
+            path: url.parse(url).path
+        }
+
+        let resp = await get(options);
+
+        // making sure request responded with 200
+        return await handleRest(200, resp);
+    } catch (err) {
+
+    }
+}
+
 module.exports = {
     get,
     post,
     del,
     handleRest,
-    handleQL
+    handleQL,
+    genericGet
 }
