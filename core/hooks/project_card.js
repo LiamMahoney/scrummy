0f32368314af46de993eb82d562865ca672f613e
LiamMahoney <liammahoney96@gmail.com>
2020-06-04 03:22:17 +0000

    progress on #58

--- core/hooks/project_card.js
+++ core/hooks/project_card.js
@@ -23,7 +23,8 @@ async function projectCard(data) {
                 return 'not implemented';
             case 'moved':
                 // issue is moved in project
-                return 'not implemented';
+                p = new ProjectCardHook(data);
+                return await p.moved();
         }   
     } catch (err) {
         throw err;
@@ -69,6 +70,38 @@ class ProjectCardHook {
             proms.push(this.moveToParentStageLabel(parent.labels));
 
             return await Promise.all(proms);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * Deletes the old stage label on the project card's parent and adds the
+     * new stage label that matches the column the project card was just moved
+     * to. Adding the new stage label will move all of the parent object's
+     * other child cards.
+     * TODO: left off here
+     * @returns {String} actions taken
+     */
+    async moved() {
+        try {
+            let proms = [];
+            // parent object of the project card
+            proms.push(request.genericGet(this.hook.project_card.content_url));
+            proms.push(await request.genericProjectGet(this.hook.project_card.column_url));
+
+            let [parent, column] = await Promise.all(proms);
+
+            if (!await this.isParentStageLabelCorrect(parent.labels, column.name)) {
+                proms = [];
+
+                proms.push(this.removeOldStageLabel(parent.labels, parent.number));
+                proms.push(this.addNewStageLabel(parent.number, column.name));
+
+                return await Promise.all(proms);
+            }
+            
+            return `project card ${this.hook.project_card.id} was already in the right column`;
         } catch (err) {
             throw err;
         }
@@ -132,7 +165,7 @@ class ProjectCardHook {
      */
     async moveToParentStageLabel(labels) {
         try {
-            let stageLabel = await this.findStageLabel(labels);
+            let stageLabel = await this.findLabelOfType(labels, 'stage');
             let stage = stageLabel.name.substr(stageLabel.name.indexOf(':') + 1).trim().toLowerCase();
             
             // finding the proejct column that matches the stage label name
@@ -154,18 +187,21 @@ class ProjectCardHook {
     }
 
     /**
-     * Finds the stage label in the list of labels given to the method.
+     * Finds the stage label in the list of labels given to the method. Assumes
+     * there is only one type of the label we are searching for in the list
+     * labels.
      * 
      * @param {Array} labels list of label objects
+     * @param {String} type the type of label to find in the list of labels
      * @returns {Object} label object that has stage in it
      * @throws {Error} when multiple stage labels are found
      */
-    async findStageLabel(labels) {
+    async findLabelOfType(labels, type) {
         try {
             let stageLabels = [];
 
             for (let label of labels) {
-                if (label.name.substr(0, label.name.indexOf(':')).toLowerCase().trim() === 'stage') {
+                if (label.name.substr(0, label.name.indexOf(':')).toLowerCase().trim() === type.toLowerCase().trim()) {
                     stageLabels.push(label);
                 }
             }
@@ -175,6 +211,98 @@ class ProjectCardHook {
             }
 
             throw Error(`couldn't find stage label`)
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * Finds the label that matches the given type and name. If I am looking
+     * for the label 'stage: To Do', the type would be 'stage' and the name
+     * would be 'To Do'.
+     * 
+     * @param {Array} labels list of labels to find a label from 
+     * @param {String} type the type of the label being searched for
+     * @param {String} name the second part of the label, e.g. if the label is
+     * 'project: Testing 1' the name would be 'Testing 1'
+     * @returns {Object} describes the matching label, if found
+     */
+    async findMatchingLabel(labels, type, name) {
+        try {
+            for (let label of labels) {
+                if (label.name.toLowerCase().trim() === `${type}: ${name}`.toLowerCase().trim()) {
+                    return label;
+                }
+            }
+
+            throw Error(`Couldn't find label that matched type: ${type} name: ${name}`);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * Compares the stage label on the parent object with the column the 
+     * project card was moved to. 
+     * 
+     * @param {Array} labels list of labels on the parent object
+     * @param {String} columnName the name of the column the project card was
+     * moved to
+     * @returns {Boolean}
+     */
+    async isParentStageLabelCorrect(labels, columnName) {
+        try {
+            // the stage label on the parent object
+            let currentStageLabel = await this.findLabelOfType(labels, 'stage');
+
+            // the stage from the stage label on the parent
+            let stageLabelStage = currentStageLabel.name.substr(currentStageLabel.name.indexOf(':') + 1).toLowerCase().trim();
+
+
+            if (columnName.toLowerCase().trim() === stageLabelStage) {
+                return true;
+            }
+
+            return false;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * 
+     * @param {Array} labels list of labels on the project card's parent 
+     * @param {int} parentNumber the issue / PR number of hte project card's
+     * parent
+     */
+    async removeOldStageLabel(labels, parentNumber) {
+        try {
+            // finding the stage label on the parent object
+            let oldStageLabel = await this.findLabelOfType(labels, 'stage');
+            
+            // removing old stage label
+            return await actions.Issue.removeLabel(parentNumber, oldStageLabel.name, this.repositoryOwner, this.repository);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * 
+     * @param {int} parentNumber the issue / PR number of the project card's 
+     * parent
+     * @param {String} columnName the name of the column the project card was 
+     * moved to
+     */
+    async addNewStageLabel(parentNumber, columnName) {
+        try {
+            let labels = await actions.Label.getAllLabels(this.repositoryOwner, this.repository);
+
+            // finding the stage label that matches the column the project card
+            // was moved to
+            let newStageLabel = await this.findMatchingLabel(labels, 'stage', columnName);
+
+            return await actions.Issue.addLabels(parentNumber, [newStageLabel.name], this.repositoryOwner, this.repository);
         } catch (err) {
             throw err;
         }
